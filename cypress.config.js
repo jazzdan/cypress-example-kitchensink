5a524dbc7185a99fc4409127e0cd5e2b8d0374ca
Dan Miller <dan@replay.io>
2023-02-27 10:29:58 -0800

    testing replay e2e

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,14 @@
-module.exports = {
+const { defineConfig } = require('cypress');
+// 🙋‍♂️ Add this line to require the replay plugin
+const replay = require("@replayio/cypress")
+
+module.exports = defineConfig({
   'projectId': '4b7344',
-  e2e: {},
-}
+  e2e: {
+    setupNodeEvents(on, config) {
+      // 🙋‍♂️ Add this line to install the replay plugin
+      replay.default(on, config);
+      return config;
+    },
+  },
+});
